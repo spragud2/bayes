fd52c7e396c62189bf1116ca8455af4b21c92e4f
Dan Sprague <spragud@gmail.com>
2021-09-12 16:51:22 -0400

    Update poisson.jl

--- poisson.jl
+++ poisson.jl
@@ -1,9 +1,13 @@
-using Turing
+using Turing, Random 
 using Distributions
 using Plots
 
 λ1 = Exponential()
 λ2 = Exponential()
+
+struct Determin{T<:Array} <: ContinuousUnivariateDistribution
+    val::T
+  end
 
 function generative()
     states = Vector{Int32}(undef,50)
@@ -22,7 +26,12 @@ states = generative()
 
 
 @model function testing(states)
-    λ1 = Exponential()
-    λ2 = Exponential()
+    λ1 ~ Exponential()
+    λ2 ~ Exponential()
+    tau ~ DiscreteUniform(1,50)
+
+    lambda_ ~ Determin([i < τ ? λ1 : λ2 for i in 1:50 ])
+
+    observations ~ Poisson(states,lambda_)
 
 end
